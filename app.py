a37059ec079e32a3d6aa3f9aef399ee49d1034cc
yayamaya <jdlove123123@gmail.com>
2024-11-14 15:36:25 +0100

    Initial commit

--- app.py
+++ app.py
@@ -12,11 +12,6 @@ with open("/Users/yayamaya/PycharmProjects/python_uppgift/tasks.json","r")as f:
     task_list = json.load(f)
 
 
-#define a category list
-categories = []
-
-tasks_under_cate = []
-
 
 #index page
 @app.route("/")
@@ -27,102 +22,114 @@ def show_index():
 #get tasks
 @app.route("/tasks",methods=["GET"])
 def show_tasks():
-    return render_template("show_taskspage_with_filter.html",task_list=task_list)
+    status = request.args.get("status")
+    filteded_task_list_pending = []
+    filteded_task_list_completed = []
+    for task in task_list:
+        if task["status"] == "pending":
+            filteded_task_list_pending.append(task)
+        elif task["status"] == "finished":
+            filteded_task_list_completed.append(task)
 
+    if status == "pending":
+        return jsonify(filteded_task_list_pending),200
+    elif status == "complete":
+        return jsonify(filteded_task_list_completed),200
+    else:
+        return jsonify(task_list),200
 
 #post a new tasks, task id is the key
 @app.route("/tasks",methods=["POST"])
 def add_task():
-    info_dict = request.get_json()
-    new_task = {"id":info_dict["id"],
-                "description":info_dict["description"],
-                "category":info_dict["category"],
-                "status":info_dict["status"]
-                }
-    for task in task_list:
+    # I added json format för att kör test program
+    info_dict = request.form or request.args or request.get_json()
+
+    try:
+        new_task = {"id":int(info_dict["id"]),
+                    "description":info_dict["description"],
+                    "category":info_dict["category"],
+                    "status":"pending" #pending is the default status
+                    }
+    except ValueError:
+        return jsonify({"error":"invalid id"}),400
+
+    for task in task_list: #check if the id is already in list
         if task["id"] == new_task["id"]:
             return jsonify({"error": "task already existed"}, 409)
     task_list.append(new_task)
-    return jsonify(new_task)
+    return jsonify(f"a new task{new_task['id']} has been added"),201
 
 #get by task id
 @app.route("/tasks/<int:id>",methods=["GET"])
 def show_task_by_id(id):
     for task in task_list:
         if task["id"] == id:
-            return jsonify(task)
-    return jsonify({"error":"task not found"},404)
+            return jsonify(task),200
+    return jsonify({"error":"task not found"}),404
 
-#delete task by id
-@app.route("/tasks/<int:id>",methods=["DELETE"])
-def delete_task_by_id(id):
-    for task in task_list:
-        if task["id"] == id:
-            task_list.remove(task)
-            return jsonify({"msg":"deleted"}),200
-    return jsonify({"error":"task not found"},404)
 
-#change a task by id
 @app.route("/tasks/<int:id>",methods=["PUT"])
 def modify_task_by_id(id):
-    description = request.args.get("description")
-    category = request.args.get("category")
+    #need ti create a dict to put the information in it, because request.get.json can not get specific key:value
+    dict_new_info = request.args or request.get_json()
+    description = dict_new_info.get("description")
+    category = dict_new_info.get("category")
     if not description and not category:
-        return jsonify({"error":"invalid data"},404)
+        return jsonify({"error":"invalid data"}),400
     for task in task_list:
         if task["id"] == id:
             if description:
                 task["description"] = description
             if category:
                 task["category"] = category
+            return jsonify("the task has been updated"),200
+    return jsonify({"error":"no such task"}),404
 
-    return jsonify({"msg":f"task {task} modified"})
 
-#put status by id
-@app.route("/tasks/<int:id>/complete",methods=["PUT"])
-def change_task_status_by_id(id):
-    status = request.args.get("status")
-    if not status:
-        return jsonify({"error": "invalid data"}, 404)
+@app.route("/tasks/<int:id>", methods=["DELETE"])
+def delete_task_by_id(id):
     for task in task_list:
         if task["id"] == id:
-            if task["status"] != status:
-                task["status"] = status
+            task_list.remove(task)
+            return jsonify("task deleted"),200
+    return jsonify({"error":"task not found"},404)
 
-    return jsonify({"msg":f"the status of {task['id']} is {task['status']}"})
+
+
+
+#put status(override the post) by id
+@app.route("/tasks/<int:id>/complete",methods=["PUT"])
+def change_task_status_by_id(id):
+    for task in task_list:
+        if task["id"] == id:
+            task["status"] = "completed"
+            return jsonify({"msg":f"the status of {task['id']} is {task['status']}"}),200
+        return jsonify({"error":"no such task"}),404
+
+
 
 #get catagory list
 @app.route("/tasks/categories",methods=["GET"])
 def show_categories():
+    # use a set to limit the same category only shows once
+    categories_set = set()
     for task in task_list:
-        categories.append(task["category"])
-    return render_template("show_categories.html",task_list=task_list)
+        categories_set.add(task["category"])
+    # change the set back to the list
+    categories = list(categories_set)
+    return jsonify(categories),200
 
 #get tasks under a specific catelist
 @app.route("/tasks/categories/<category>",methods=["GET"])
 def show_tasklist_by_cate(category):
     tasks_under_cate = []
-    print(category)
     for task in task_list:
-        print(task)
         if task["category"] == category:
             tasks_under_cate.append(task)
-    print(tasks_under_cate)
-    return render_template("show_specific_category.html",task_list=tasks_under_cate,category=category)
+            return jsonify(tasks_under_cate),200
+    return jsonify("no such category"),404
 
-#submit function
-@app.route("/submit", methods=["POST"])
-def submitt_formular():
-    id = request.form.get("id")
-    for task in task_list:
-        if task["id"] == id:
-            return f"the task id alreafy existed!",409
-    description = request.form.get("description")
-    category = request.form.get("category")
-    status = request.form.get("status")
-    new_task = {"id":id,"description":description,"category":category,"status":status}
-    task_list.append(new_task)
-    return render_template("task_added.html",new_task=new_task)
+
 
 
 
---
+++ test_app.py
@@ -0,0 +1,65 @@
+
+
+import pytest
+from app import app
+
+
+#inplement a client server to test
+@pytest.fixture()
+def client():
+    app.config["Testing"] = True
+    with app.test_client() as client:
+        #the client server's status will be purged after yield
+        yield client
+
+
+def test_get_tasks(client):
+    response = client.get("/tasks")
+    print(response.get_json())
+    assert response.status_code == 200
+
+def test_post_a_new_task_on_tasks(client):
+    response = client.post("/tasks",json={
+        "id":125,
+        "description":"Buy more ice cream!",
+        "category":"Shopping",
+        "status":"pending"
+    })
+    print(response.get_json())
+    assert response.status_code == 201
+
+def test_get_task_by_id(client):
+    response = client.get("/tasks/125")
+    print(response.get_json())
+    assert response.status_code == 200
+
+def test_modify_task_by_id_json(client):
+    response = client.put("tasks/125",json={
+        "description":"Water the plants",
+        "category":"Watering"
+    })
+    print(response.get_json())
+    assert response.status_code == 200
+
+def test_modify_task_by_id_url(client):
+    response = client.put("/tasks/125?description=Water+The+Plants&category=Watering")
+    print(response.get_json())
+    assert response.status_code == 200
+
+def test_delete_task_by_id(client):
+    response = client.delete("tasks/125")
+    assert response.status_code == 200
+
+def test_change_task_status_by_id(client):
+    response = client.put("/tasks/123/complete")
+    assert response.status_code == 200
+
+def test_show_categories(client):
+    response = client.get("/tasks/categories")
+    assert response.status_code == 200
+    assert isinstance(response.json,list)
+
+def test_show_tasklist_by_cate(client):
+    response = client.get("/tasks/categories/Cleaning")
+    print(response.get_json())
+    assert response.status_code == 200
